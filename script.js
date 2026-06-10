472d1002ff7cb12b4a78dea97d8ffcac8d87f209
Abhi Rajendran <24395ar@hvhs.school.nz>
2026-06-10 13:30:59 +1200

    adding images

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello world!")
+console.log("Working?")
+
+
+const images =["cafeone.png","cafetwo.png","cafethree.png"];
+
+let currentImage = 0;
+
+const imageElement = document.getElementById("Slideshow")
+
+function changeImage(){
+imageElement.classList.add("fade-out")
+
+currentImage = (currentImage + 1) % images.length;
+imageElement.src = images[currentImage];
+
+}
+
+setInterval(changeImage, 4000);
