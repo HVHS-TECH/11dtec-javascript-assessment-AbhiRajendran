deb07c0c4b5e2c395e17b57ded27ae16d836f243
Abhi Rajendran <24395ar@hvhs.school.nz>
2026-06-15 09:54:50 +1200

    added the transition

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ console.log("Working?")
 
 
 const images =["cafeone.png","cafetwo.png","cafethree.png"];
-
 let currentImage = 0;
 
 const imageElement = document.getElementById("Slideshow")
 
 function changeImage(){
-imageElement.classList.add("fade-out")
-
+imageElement.classList.add("fade-out");
+setTimeout(() => {
 currentImage = (currentImage + 1) % images.length;
 imageElement.src = images[currentImage];
-
+imageElement.classList.remove("fade-out");
+}, 500);
 }
 
 setInterval(changeImage, 4000);
