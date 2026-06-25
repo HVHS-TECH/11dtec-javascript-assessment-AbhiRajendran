21cca91c68afa96e8e32a5fd5d0890d824b20824
Abhi Rajendran <24395ar@hvhs.school.nz>
2026-06-26 11:59:32 +1200

    Making the add button work

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+/*Checking*/
+
 console.log("Working?")
 
+/*SlideShow Transition*/
 
 const images =["coffee.jpg","tea.jpg","cafethree.png"];
 let currentImage = 0;
-
 const imageElement = document.getElementById("Slideshow")
-
 function changeImage(){
 imageElement.classList.add("fade-out");
 setTimeout(() => {
@@ -14,5 +15,18 @@ imageElement.src = images[currentImage];
 imageElement.classList.remove("fade-out");
 }, 500);
 }
-
 setInterval(changeImage, 4000); 
+
+/*Featured Item*/
+ 
+function addQuantity(id, name) {
+    let quantity = document.getElementById(id).value;
+
+    if (quantity <= 0 || quantity === "") {
+        alert("Please enter a quantity.");
+        return;
+    }
+
+    alert(quantity + " " + name + "(s) added to cart!");
+}
+
